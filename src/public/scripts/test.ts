76441ca7710ea348a797ae80fa45a68877381cf6
Tim Stott <ta.stott@googlemail.com>
2017-04-03 19:42:15 +0100

    Bit of tidying up

--- src/public/scripts/test.ts
+++
@@ -1,7 +0,0 @@
-const es = new EventSource("/outgoing");
-
-es.onmessage = (event: sse.IOnMessageEvent)  => {
-    document.body.innerHTML = JSON.stringify(event.data, null, 4);
-};
-
-// http://25.media.tumblr.com/fadd7fc5fc867696a287520bb06b09ae/tumblr_n037nomlwN1r8exu0o1_250.gif
